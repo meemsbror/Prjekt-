592e42a67ff80569003eff842aacc5ed6cb9e629
mau5man <axel.bergrahm@live.com>
2016-03-30 14:00:03 +0200

    Add empty AbstractCharacter & Character classes

---
+++ gamecode/src/AbstractCharacter.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Axel on 16/03/30.
+ */
+public abstract class AbstractCharacter {
+
+    //arbitrary 100
+    private int hitPoints = 100;
+
+    public void move(){
+
+    }
+}
---
+++ gamecode/src/Character.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Axel on 16/03/30.
+ */
+public class Character extends AbstractCharacter {
+    
+}
