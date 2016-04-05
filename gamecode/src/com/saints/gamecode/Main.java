647609ad04590ee0d3c2229d01c290bb1696ff82
Alle <Algotify@gmail.com>
2016-04-05 11:15:25 +0200

    add Main (i forgot in the last one)

---
+++ gamecode/src/com/saints/gamecode/Main.java
@@ -0,0 +1,10 @@
+package com.saints.gamecode;
+
+/**
+ * Created by admin on 2016-04-05.
+ */
+public class Main {
+    public static void main(String [] arsgs){
+        Arena arena = new Arena(CharacterFactory.createCharacter(),CharacterFactory.createCharacter());
+    }
+}
