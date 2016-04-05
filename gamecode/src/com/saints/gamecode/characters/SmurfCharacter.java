3846b501d809caf6fa762a9c7d42df5e4f195ca9
Alle <Algotify@gmail.com>
2016-04-05 11:12:30 +0200

    add image to smurfCharacter and a Main class

--- gamecode/src/com/saints/gamecode/characters/SmurfCharacter.java
+++ gamecode/src/com/saints/gamecode/characters/SmurfCharacter.java
@@ -2,8 +2,8 @@ package com.saints.gamecode.characters;
 
 public class SmurfCharacter extends Character {
 
-    public SmurfCharacter(int x, int y){
-
-        super(x,y);
+    public SmurfCharacter(int x, int y, int width, int height){
+        super(x,y,width,height);
+        setImgPath("/Users/admin/Documents/Prjekt-/assets/pictures/smurf1.png");
     }
 }
