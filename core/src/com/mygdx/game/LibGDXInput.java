7aa0bd222d428a923b33151125da401f8d5a3556
Frej Karlsson <frejlekarlsson@gmail.com>
2016-04-16 21:15:55 +0200

    Add direction to key map

--- core/src/com/mygdx/game/LibGDXInput.java
+++ core/src/com/mygdx/game/LibGDXInput.java
@@ -3,32 +3,38 @@ package com.mygdx.game;
 import com.badlogic.gdx.Input;
 import com.saints.gamecode.Direction;
 import com.saints.gamecode.interfaces.IKeyInput;
-import com.sun.javafx.collections.MappingChange;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LibGDXInput implements IKeyInput {
 
-    public MappingChange.Map<Direction,Input.Keys> map;
+    public Map<Direction,Integer> map;
     private final Input input;
 
     public LibGDXInput (Input input){
 
         this.input = input;
+        initiateDirection();
     }
 
 
     public boolean isKeyPressed(Direction direction){
-
-
         return input.isKeyPressed(translateKey(direction));
     }
 
 
     private int translateKey(Direction direction){
-        return 5;
+        return map.get(direction);
     }
 
 
-    private void initiate(){
+    private void initiateDirection(){
+        map = new HashMap<Direction,Integer>();
 
+        map.put(Direction.P1LEFT,Input.Keys.LEFT);
+        map.put(Direction.P1RIGHT,Input.Keys.RIGHT);
+        map.put(Direction.P1DIVE,Input.Keys.DOWN);
+        map.put(Direction.P1JUMP,Input.Keys.UP);
     }
 }
