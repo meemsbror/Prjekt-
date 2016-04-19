695a47cd4c90657428545daef0687f4c3fedbcc5
Frej Karlsson <frejlekarlsson@gmail.com>
2016-04-19 17:21:49 +0200

    forgot to add file

---
+++ gamecode/src/com/saints/gamecode/Physics.java
@@ -0,0 +1,15 @@
+package com.saints.gamecode;
+
+public class Physics {
+
+    private final Vector2D GRAVITY = new Vector2D(0,0.92f);
+    private Vector2D deltaGravity;
+    private float delta;
+
+    public void applyGravity(float delta, Vector2D vector){
+        deltaGravity = (Vector2D)GRAVITY.clone();
+        deltaGravity.multiplyVector(delta);
+        vector.addVector(deltaGravity);
+    }
+    
+}
