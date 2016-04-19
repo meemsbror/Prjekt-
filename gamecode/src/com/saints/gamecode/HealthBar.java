981f24cbde88d4f156b0798055904cc56920f2c8
mau5man <axel.bergrahm@live.com>
2016-04-19 16:34:44 +0200

    Make HealthBar.java a Singleton

--- gamecode/src/com/saints/gamecode/HealthBar.java
+++ gamecode/src/com/saints/gamecode/HealthBar.java
@@ -3,8 +3,42 @@ package com.saints.gamecode;
 /**
  * Created by Axel on 16/04/19.
  */
-public class HealthBar {
-    private int currentMax;
+public final class HealthBar {
 
-    
+    private int currentMax;
+    private int divider;
+
+    /*
+    We use the singleton design pattern in order to make sure that we can
+    only have a single Health-Bar at any time
+     */
+    private static volatile HealthBar instance;
+    private HealthBar() { }
+
+    public static HealthBar getInstance() {
+        if (instance == null ) {
+            synchronized (HealthBar.class) {
+                if (instance == null) {
+                    instance = new HealthBar();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+
+
+    // Method for setting the current HP bar's max
+    public void setMax(int x){
+        this.currentMax += x;
+    }
+
+    // returns
+    public int getMax(){
+        return currentMax;
+    }
+
+
+
 }
