b38fadb7632521486cdf89300b1e07e6cbf2d54e
mau5man <axel.bergrahm@live.com>
2016-04-20 10:09:57 +0200

    add relevant TODO's and temporary setDivider setting to currentMax -50

--- gamecode/src/com/saints/gamecode/HealthBar.java
+++ gamecode/src/com/saints/gamecode/HealthBar.java
@@ -16,8 +16,10 @@ public final class HealthBar {
 
 
     // TODO: set current max to player 1 + player 2 or just have it decreased until some player dies
+    // arbitrary 100% for now. May come to change
     private int currentMax = 100;
     // TODO: set divider to currentMax - Player1's HP
+    // HP-bar divider variable
     private int divider;
 
 
@@ -33,14 +35,16 @@ public final class HealthBar {
     }
 
     //Getters & setters for divider variable
+    //TODO: set the divider to the difference between currentMax & p1 or p2 & currentMax
     public void setDivider(){
-        this.divider = currentMax - Player1.getHP();
+        this.divider = currentMax - 50; // - Player1.getHP();
     }
     public int getDivider(){
         return divider;
     }
 
-    // TODO: check for individual Character health
+    // TODO: check for individual Character health,
+    // TODO: make connection for whenever a Character is hit, this check will be carried out
     // boolean check for if either player has run out of HP
     public boolean isOver(){
         if (divider <= 0 || divider > currentMax){
