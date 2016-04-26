e9018ae0a90ec15064fafb270f4a5623776c7d7a
mau5man <axel.bergrahm@live.com>
2016-04-26 18:50:12 +0200

    add test for getters & setters

--- gamecode/src/test/java/testList/HealthTest.java
+++ gamecode/src/test/java/testList/HealthTest.java
@@ -5,10 +5,41 @@ import org.junit.Test;
 
 import static junit.framework.TestCase.assertTrue;
 
-/**
- * Created by admin on 2016-04-26.
- */
+// Class for testing HealthBar.java
 public class HealthTest {
+
+    @Test
+    public void getterTest(){
+        // basic values set in Healthbar class, divider to 40 and maxHealth to 100 for testing.
+        HealthBar healthBar = HealthBar.getInstance();
+
+        // test for divider
+        int currentDivider;
+        currentDivider = healthBar.getDivider();
+        assertTrue(currentDivider == 40);
+
+        // test for max health
+        int maxHealth;
+        maxHealth = healthBar.getMaxHealth();
+        assertTrue(maxHealth == 100);
+    }
+
+    @Test
+    public void setterTest(){
+        // basic set values input
+        HealthBar healthBar = HealthBar.getInstance();
+
+        // test for setting divider
+        healthBar.setDivider(80);
+        int currentDivider = healthBar.getDivider();
+        assertTrue(currentDivider == 80);
+
+        //test for setting max health
+        healthBar.setMaxHealth(35);
+        int maxHealth = healthBar.getMaxHealth();
+        assertTrue(maxHealth == 35);
+    }
+
     @Test
     public void takeDamageTest(){
         // player 1 deal dmg
