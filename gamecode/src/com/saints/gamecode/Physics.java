ea9975c8ff20fc19da5486121baca27d4c46000d
Frej Karlsson <frejlekarlsson@gmail.com>
2016-05-03 16:46:38 +0200

    clean up physics

--- gamecode/src/com/saints/gamecode/Physics.java
+++ gamecode/src/com/saints/gamecode/Physics.java
@@ -2,28 +2,33 @@ package com.saints.gamecode;
 
 import com.saints.gamecode.gameobjects.GameObject;
 
+//Singleton class that does basic Physics calculations
 public class Physics {
 
+    //Sets the gravity of the world
     private final Vector2D GRAVITY = new Vector2D(0,-98.2f);
-    private Vector2D deltaGravity;
+
+    //The only instance of this class
     private static final Physics instance = new Physics();
 
 
     private Physics (){
-
     }
 
+    //Returns the instance of this singleton
     public static Physics getInstance(){
         return instance;
     }
 
+    //Gets the acceleration caused by gravity given in a specified time (delta)
     public Vector2D getGravity(float delta){
-        deltaGravity = (Vector2D)GRAVITY.clone();
+        Vector2D deltaGravity = (Vector2D)GRAVITY.clone();
         deltaGravity.multiplyVector(delta);
         return deltaGravity;
     }
 
-    public boolean hasColided(GameObject object1, GameObject object2){
+    //Checks if two objects have collided
+    public boolean hasCollided(GameObject object1, GameObject object2){
 
         if(object1.getPos().getX()+object1.getPos().getX()<object2.getPos().getX() ||object2.getPos().getX()+object2.getWidth()<object1.getPos().getX()
                 ||object1.getPos().getY()+object1.getHeight()<object2.getPos().getY() ||object2.getPos().getY()+object2.getHeight()<object1.getPos().getY()){
@@ -31,10 +36,5 @@ public class Physics {
         }else{
             return true;
         }
-    }
-    public void applyGravity(float delta, Vector2D vector){
-        deltaGravity = (Vector2D)GRAVITY.clone();
-        deltaGravity.multiplyVector(delta);
-        vector.addVector(deltaGravity);
     }
 }
