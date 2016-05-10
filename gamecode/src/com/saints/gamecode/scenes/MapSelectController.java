4c62cc6ecfac7f6c1563292f99f881ac46457d88
Frej Karlsson <frejlekarlsson@gmail.com>
2016-05-10 15:40:26 +0200

    add basic mapselectcontroller

---
+++ gamecode/src/com/saints/gamecode/scenes/MapSelectController.java
@@ -0,0 +1,11 @@
+package com.saints.gamecode.scenes;
+
+import com.saints.gamecode.interfaces.IScene;
+
+public class MapSelectController implements IScene{
+
+    public void update(float delta){
+
+    }
+
+}
