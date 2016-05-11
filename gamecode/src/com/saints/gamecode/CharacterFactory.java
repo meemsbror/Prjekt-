8157bca4d3a99095c7a34851c462c4a9cb259b7b
Frej Karlsson <frejlekarlsson@gmail.com>
2016-05-11 17:02:45 +0200

    add getCharacters method

--- gamecode/src/com/saints/gamecode/CharacterFactory.java
+++ gamecode/src/com/saints/gamecode/CharacterFactory.java
@@ -5,9 +5,8 @@ import com.saints.gamecode.gameobjects.characters.Character;
 
 public class CharacterFactory {
 
-    public static Character createCharacter(String name){
-        //Singelton-ish pattern to place players on different places?
-        /*switch (name){
+    public static Character createCharcter(String name){
+       /*switch (name){
             case "Smurf" :
                 return new SmurfCharacter(0,0);
             default:
@@ -15,5 +14,10 @@ public class CharacterFactory {
                 */
         if (name == "Smurf") {return new SmurfCharacter(200,50);}
         else {return new SmurfCharacter(100,50);}
+    }
+
+    //Returns an array with the name of all available characters
+    public String [] getCharacters(){
+        return new String [] {"smurf"};
     }
 }
