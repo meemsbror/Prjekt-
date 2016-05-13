30f8826d953172960e4a275fcd8f545b411b17c7
Frej Karlsson <frejlekarlsson@gmail.com>
2016-05-13 15:03:38 +0200

    build CharacterSelect further

---
+++ gamecode/src/com/saints/gamecode/CharacterPanel.java
@@ -0,0 +1,22 @@
+package com.saints.gamecode;
+
+public class CharacterPanel {
+
+    private boolean player1Selected, player2Selected;
+
+    public boolean isPlayer1Selected() {
+        return player1Selected;
+    }
+
+    public void setPlayer1Selected(boolean player1Selected) {
+        this.player1Selected = player1Selected;
+    }
+
+    public boolean isPlayer2Selected() {
+        return player2Selected;
+    }
+
+    public void setPlayer2Selected(boolean player2Selected) {
+        this.player2Selected = player2Selected;
+    }
+}
