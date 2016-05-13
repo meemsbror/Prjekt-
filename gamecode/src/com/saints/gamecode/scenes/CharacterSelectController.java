cf8fcba68219cc33cfeeda0fed525e07d596a74e
Frej Karlsson <frejlekarlsson@gmail.com>
2016-05-13 15:03:38 +0200

    build CharacterSelect further

--- gamecode/src/com/saints/gamecode/scenes/CharacterSelectController.java
+++ gamecode/src/com/saints/gamecode/scenes/CharacterSelectController.java
@@ -1,10 +1,13 @@
 package com.saints.gamecode.scenes;
 
 import com.saints.gamecode.CharacterFactory;
+import com.saints.gamecode.CharacterPanel;
 import com.saints.gamecode.Direction;
+import com.saints.gamecode.Position;
 import com.saints.gamecode.gameobjects.characters.Character;
 import com.saints.gamecode.interfaces.IGraphics;
 import com.saints.gamecode.interfaces.IKeyInput;
+import javafx.geometry.Pos;
 
 public class CharacterSelectController extends Scene{
 
@@ -12,15 +15,19 @@ public class CharacterSelectController extends Scene{
     private final IGraphics graphics;
     private Direction [] directions;
     private String char1, char2;
+    private CharacterPanel characterPanels[][];
+    private Position p1Pos, p2Pos;
 
     public CharacterSelectController(IKeyInput input, IGraphics graphics){
         this.input = input;
         this.graphics = graphics;
         initiateInputArray();
+        initiatePositions();
     }
 
     public void update(float delta) {
         checkInput();
+        updateActivePanels();
         char1 = "Smurf";
         char2 = "Smurf";
         firePropertyChange("characters",null,null);
@@ -34,28 +41,74 @@ public class CharacterSelectController extends Scene{
         }
     }
 
+    //Iterates all panels and activates the ones selected
+    private void updateActivePanels(){
+        for(int i = 0; i<characterPanels.length; i++){
+            for(int j=0; j<characterPanels[i].length; j++){
+                CharacterPanel characterPanel = characterPanels[i][j];
+
+                characterPanel.setPlayer1Selected(((int)p1Pos.getY() == i && (int)p1Pos.getX() == j));
+                characterPanel.setPlayer2Selected(((int)p2Pos.getY() == i && (int)p2Pos.getX() == j));
+            }
+        }
+    }
+
+    //tells the position to update depending on what the input is
     private void keyPressed(Direction dir){
 
         switch (dir){
             case P1JUMP:
+                moveUP(p1Pos);
                 break;
             case P1LEFT:
+                moveLeft(p1Pos);
                 break;
             case P1RIGHT:
+                moveRight(p1Pos);
                 break;
             case P1DIVE:
+                moveDown(p1Pos);
                 break;
 
             case P2JUMP:
+                moveUP(p2Pos);
                 break;
             case P2LEFT:
+                moveLeft(p2Pos);
                 break;
             case P2RIGHT:
+                moveRight(p2Pos);
                 break;
             case P2DIVE:
+                moveDown(p2Pos);
                 break;
-            default:
-                break;
+        }
+    }
+
+    private void moveUP(Position pos){
+        if(pos.getY()>0){
+            pos.move(0,-1);
+        }
+    }
+
+    private void moveDown(Position pos){
+        int height = characterPanels.length;
+        if(pos.getY()<height-1){
+            if(!((int)pos.getX() > characterPanels[(int)pos.getY() + 1].length - 1)){
+                pos.move(0,1);
+            }
+        }
+    }
+
+    private void moveRight(Position pos){
+        if(pos.getX()<characterPanels[(int)pos.getY()].length-1){
+            pos.move(1,0);
+        }
+    }
+
+    private void moveLeft(Position pos){
+        if(pos.getX()>0){
+            pos.move(-1,0);
         }
     }
 
@@ -65,6 +118,11 @@ public class CharacterSelectController extends Scene{
 
     public Character getPlayer2(){
         return CharacterFactory.createCharacter(char2);
+    }
+
+    private void initiatePositions(){
+        p1Pos = new Position(0,0);
+        p2Pos = new Position(0,0);
     }
 
     private void initiateInputArray(){
