8248894708103af63968deaca5d557e6494a0f05
Frej Karlsson <frejlekarlsson@gmail.com>
2016-05-18 15:07:59 +0200

    change imgPath to be declared in constructor

--- gamecode/src/com/saints/gamecode/CharacterPanel.java
+++ gamecode/src/com/saints/gamecode/CharacterPanel.java
@@ -4,7 +4,12 @@ public class CharacterPanel {
 
     private boolean player1Selected, player2Selected;
 
-    private String imgPath;
+    private final String imgPath, characterName;
+
+    public CharacterPanel(String imgPath, String characterName){
+        this.imgPath = imgPath;
+        this.characterName = characterName;
+    }
 
     public boolean isPlayer1Selected() {
         return player1Selected;
@@ -20,13 +25,5 @@ public class CharacterPanel {
 
     public void setPlayer2Selected(boolean player2Selected) {
         this.player2Selected = player2Selected;
-    }
-
-    public void setImage(String imgPath){
-        this.imgPath = imgPath;
-    }
-
-    public String getImage(){
-        return this.imgPath;
     }
 }
