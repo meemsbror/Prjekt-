363f64b587c046a8b8708d8fd2ec85f9e6b9b428
Frej Karlsson <frejlekarlsson@gmail.com>
2016-05-18 14:31:23 +0200

    add setImage

--- gamecode/src/com/saints/gamecode/CharacterPanel.java
+++ gamecode/src/com/saints/gamecode/CharacterPanel.java
@@ -4,6 +4,8 @@ public class CharacterPanel {
 
     private boolean player1Selected, player2Selected;
 
+    private String imgPath;
+
     public boolean isPlayer1Selected() {
         return player1Selected;
     }
@@ -18,5 +20,13 @@ public class CharacterPanel {
 
     public void setPlayer2Selected(boolean player2Selected) {
         this.player2Selected = player2Selected;
+    }
+
+    public void setImage(String imgPath){
+        this.imgPath = imgPath;
+    }
+
+    public String getImage(){
+        return this.imgPath;
     }
 }
