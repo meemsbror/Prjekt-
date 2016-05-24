96850c942f82c203990f890cd7bc1ed8cd0b7e4b
mau5man <axel.bergrahm@live.com>
2016-05-24 14:30:40 +0200

    add EndGameScene class in Scenes module

---
+++ gamecode/src/com/saints/gamecode/scenes/EndGameScene.java
@@ -0,0 +1,11 @@
+package com.saints.gamecode.scenes;
+
+public class EndGameScene extends Scene {
+	@Override
+	public void update(float delta) {
+
+	}
+	public EndGameScene(String winner){
+
+	}
+}
