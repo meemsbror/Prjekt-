ac4278765560af3da7a110408ad78b3dbd490d3c
mau5man <axel.bergrahm@live.com>
2016-05-24 16:19:35 +0200

    add WinnerScreenClass (incomplete)

---
+++ gamecode/src/com/saints/gamecode/WinnerScreen.java
@@ -0,0 +1,30 @@
+package com.saints.gamecode;
+
+import com.saints.gamecode.interfaces.IEntity;
+
+public class WinnerScreen implements IEntity {
+
+	private Position pos;
+
+	private int width = 400;
+	private int height = 300;
+	@Override
+	public void setPosition(float x, float y) {
+
+	}
+
+	@Override
+	public Position getPosition() {
+		return null;
+	}
+
+	@Override
+	public int getWidth() {
+		return 0;
+	}
+
+	@Override
+	public int getHeight() {
+		return 0;
+	}
+}
--- gamecode/src/com/saints/gamecode/scenes/EndGameScene.java
+++ gamecode/src/com/saints/gamecode/scenes/EndGameScene.java
@@ -1,11 +1,41 @@
 package com.saints.gamecode.scenes;
 
+import com.saints.gamecode.interfaces.IGraphics;
+import org.lwjgl.Sys;
+
+import java.util.Timer;
+
 public class EndGameScene extends Scene {
-	@Override
-	public void update(float delta) {
+	private final long startTime;
+	private String winner;
+	private final IGraphics graphics;
+
+	public EndGameScene(String winner, IGraphics graphics){
+		this.startTime = System.currentTimeMillis();
+		this.graphics = graphics;
+		setWinner(winner);
 
 	}
-	public EndGameScene(String winner){
 
+	private void setWinner(String winner){
+		if (winner.equals("player 1")){
+			this.winner = winner;
+		}if (winner.equals("player 2")){
+			this.winner = winner;
+		}else{
+			this.winner = "Lars";
+		}
+	}
+
+
+	@Override
+	public void update(float delta) {
+		if (startTime + 5000 < System.currentTimeMillis()){
+			firePropertyChange("new game",null,null);
+		}else if (getWinner().equals("player 1")){ // when 5 seconds have pass draw getWinner
+
+		}else if (getWinner().equals("player 2")){ // -''-
+
+		}
 	}
 }
