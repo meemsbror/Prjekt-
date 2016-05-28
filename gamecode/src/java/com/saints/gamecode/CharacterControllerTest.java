5774ab20055067718e59add5634254b262c51535
Frej Karlsson <frejlekarlsson@gmail.com>
2016-05-28 20:41:18 +0200

    For unknown reasons my coderows are increased drastictly

---
+++ gamecode/src/java/com/saints/gamecode/CharacterControllerTest.java
@@ -0,0 +1,46 @@
+package com.saints.gamecode;
+
+import com.saints.gamecode.Entities.gameobjects.characters.Character;
+import com.saints.gamecode.controllers.CharacterController;
+import com.saints.gamecode.interfaces.IEntity;
+import com.saints.gamecode.interfaces.IGraphics;
+import com.saints.gamecode.interfaces.IKeyInput;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+
+public class CharacterControllerTest {
+
+    @Test
+    public void moveMentTest(){
+
+        InputSimulator input = new InputSimulator();
+        IGraphics graphics = new GraphicsSim();
+
+        CharacterController charcont = new CharacterController(new ArrayList<IEntity>(), input, new ArrayList<>(), graphics);
+
+        Character p1 = CharacterFactory.createCharacter("Smurf",true);
+        Character p2 = CharacterFactory.createCharacter("Lucky",false);
+
+        charcont.setCharacters(p1,p2);
+
+        Position pos1 = p1.getPosition();
+        Position pos2 = p2.getPosition();
+
+
+
+        input.setCurrentKey(Direction.P1JUMP);
+        charcont.update(100);
+        assert p1.isAirborne();
+        assert !p1.getPos().equals(pos1);
+
+        input.setCurrentKey(Direction.P2JUMP);
+        charcont.update(100);
+        assert p2.isAirborne();
+        assert !p2.getPos().equals(pos2);
+    }
+
+}
